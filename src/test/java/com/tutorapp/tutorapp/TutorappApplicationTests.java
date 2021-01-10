41f0fcfbe735ca53753732e4bd123e458eb44349
neha-madaram <nehamadaram32@gmail.com>
2021-01-10 17:13:35 -0500

    add files

---
+++ src/test/java/com/tutorapp/tutorapp/TutorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorapp.tutorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
